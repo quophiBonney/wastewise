b3be92b878d687ca4862589ca043e1a03b6d8183
quophiBonney <quophiclef@gmail.com>
2025-05-13 12:07:55 +0000

    removed the redundant dbConnection invocation

--- src/pages/api/bins/request/update/index.js
+++ src/pages/api/bins/request/update/index.js
@@ -11,31 +11,5 @@ export default async function handler(req, res) {
   await dbConnect();
   return updateBinRequestStatus(req, res);
 }
-import dbConnect from "@/utils/db";
-import { updateBinRequestStatus } from "@/controllers/binRequest";
 
-/**
- * Handles the PUT request to update bin request status.
- * 
- * @param {Object} req - The incoming request object.
- * @param {Object} res - The outgoing response object.
- */
-export default async function handler(req, res) {
-  // Check if the request method is PUT, if not return a 405 status code.
-  if (req.method !== "PUT") {
-    res.setHeader("Allow", ["PUT"]);
-    return res.status(405).end("Method Not Allowed");
-  }
 
-  try {
-    // Establish a connection to the database.
-    await dbConnect();
-    
-    // Update the bin request status.
-    return updateBinRequestStatus(req, res);
-  } catch (error) {
-    // Log the error and return a 500 status code.
-    console.error(error);
-    return res.status(500).end("Internal Server Error");
-  }
-}
